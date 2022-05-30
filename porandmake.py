4651938febbaafd5d6a97db776c9b8316400f52a
Iyarr <8.24ridiculous@gmail.com>
2022-05-30 11:18:00 +0900

    修正

--- porandmake.py
+++ porandmake.py
@@ -4,12 +4,12 @@ def syntaxget():
     syntax = syntax.replace(' ','')
     syntax = syntax.replace(' ','*')
     return porandmake(syntax)
-# 逆ポーランド記法に変換するプログラム
-def porandmake(syntax):
+#   逆ポーランド記法への変換
+def porandmake(self,syntax):
     length = len(syntax)
     if length < 2:
-       return syntax
-        #
+        return syntax
+    #
     ct = 0
     deep = 0
     while ct < length:
@@ -17,31 +17,31 @@ def porandmake(syntax):
             deep = deep + 1
         elif syntax[ct] == ')':
             deep = deep - 1
-                
+            
         if deep == 0:
             if ct >= length-1:
                 syntax = syntax[1:length-1]
                 length = length - 2
             break
         ct = ct + 1
-        #
+    #
     sign = 0
     deep = 0
     ct = 0
     while ct < length:
         if syntax[ct] == '(':
-            deep = deep + 1
+           deep = deep + 1
         elif syntax[ct] == ')':
-            deep = deep - 1
+                deep = deep - 1
 
         elif deep == 0:
             if syntax[ct] == '+' or syntax[ct] == '-':
-                former = porandmake(syntax[0:ct])
-                latter = porandmake(syntax[ct+1:length])
+                former = self.porandmake(self,syntax[0:ct])
+                latter = self.porandmake(self,syntax[ct+1:length])
                 sign = 1
                 return [former,latter,syntax[ct]]
         ct = ct + 1
-        #
+    #
     ct = 0
     if sign == 0:
         level = 0
@@ -56,7 +56,7 @@ def porandmake(syntax):
                     level = level + 1
                     if level == 1:
                         st = ct
-                        
+                    
             elif deep == 0 :
                 if ct + 1 < length and syntax[ct].isdigit():
                     if syntax[ct+1].isdigit() == False:
@@ -68,10 +68,13 @@ def porandmake(syntax):
                     if level == 1:
                         st = ct
 
-
-            if level == 2:
-                former = porandmake(syntax[0:st+1])
-                latter = porandmake(syntax[st+1:length])
+            if level == 2 :
+                former = self.porandmake(self,syntax[0:st+1])
+                latter = self.porandmake(self,syntax[st+1:length])
+                return [former,latter,'*']
+            elif syntax[ct] == '*':
+                former = self.porandmake(self,syntax[0:ct])
+                latter = self.porandmake(self,syntax[ct+1:length])
                 return [former,latter,'*']
             ct = ct + 1
     return syntax
